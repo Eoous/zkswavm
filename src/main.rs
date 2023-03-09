8ed7064878310aed8920af53f45d1b4867d914e3
0xEclair <38656355+0xEclair@users.noreply.github.com>
2023-03-09 11:05:15 +0800

    doc: halo2 simple example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,224 @@
-fn main() {
-    println!("Hello, world!");
+use std::marker::PhantomData;
+
+use halo2_proofs::{
+    arithmetic::FieldExt,
+    circuit::{AssignedCell, Chip, Layouter, Region, SimpleFloorPlanner, Value},
+    plonk::{Advice, Circuit, Column, ConstraintSystem, Error, Fixed, Instance, Selector},
+    poly::Rotation,
+};
+
+trait NumericInstructions<F: FieldExt>: Chip<F> {
+    type Num;
+
+    fn load_private(&self, layouter: impl Layouter<F>, a: Value<F>) -> Result<Self::Num, Error>;
+    fn load_constant(&self, layouter: impl Layouter<F>, constant: F) -> Result<Self::Num, Error>;
+
+    fn mul(&self, layouter: impl Layouter<F>, a: Self::Num, b: Self::Num) -> Result<Self::Num, Error>;
+    fn expose_public(&self, layouter: impl Layouter<F>, num: Self::Num, row: usize) -> Result<(), Error>;
 }
+
+struct FieldChip<F: FieldExt> {
+    config: FieldConfig,
+    _marker: PhantomData<F>,
+}
+
+impl<F: FieldExt> Chip<F> for FieldChip<F> {
+    type Config = FieldConfig;
+    type Loaded = ();
+
+    fn config(&self) -> &Self::Config {
+        &self.config
+    }
+    fn loaded(&self) -> &Self::Loaded {
+        &()
+    }
+}
+
+#[derive(Clone, Debug)]
+struct FieldConfig {
+    advice: [Column<Advice>; 2],
+    instance: Column<Instance>,
+
+    s_mul: Selector,
+}
+
+impl <F: FieldExt> FieldChip<F> {
+    fn construct(config: <Self as Chip<F>>::Config) -> Self {
+        Self {
+            config,
+            _marker: PhantomData,
+        }
+    }
+
+    fn configure(
+        meta: &mut ConstraintSystem<F>,
+        advice: [Column<Advice>; 2],
+        instance: Column<Instance>,
+        constant: Column<Fixed>,
+    ) -> <Self as Chip<F>>::Config {
+        meta.enable_equality(instance);
+        meta.enable_constant(constant);
+        for col in &advice {
+            meta.enable_equality(*col);
+        }
+        let s_mul = meta.selector();
+
+        meta.create_gate("mul", |meta| {
+            let lhs = meta.query_advice(advice[0], Rotation::cur());
+            let rhs = meta.query_advice(advice[1], Rotation::cur());
+            let out = meta.query_advice(advice[0], Rotation::next());
+            let s_mul = meta.query_selector(s_mul);
+
+            vec![s_mul * (lhs * rhs - out)]
+        });
+
+        FieldConfig {
+            advice,
+            instance,
+            s_mul,
+        }
+    }
+}
+
+#[derive(Clone)]
+struct Number<F: FieldExt>(AssignedCell<F, F>);
+
+impl<F: FieldExt> NumericInstructions<F> for FieldChip<F> {
+    type Num = Number<F>;
+
+    fn load_private(
+        &self,
+        mut layouter: impl Layouter<F>,
+        value: Value<F>,
+    ) -> Result<Self::Num, Error> {
+        let config = self.config();
+
+        layouter.assign_region(
+            || "load private",
+            |mut region| {
+                region
+                    .assign_advice(|| "private input", config.advice[0], 0, || value)
+                    .map(Number)
+            },
+        )
+    }
+
+    fn load_constant(&self, mut layouter: impl Layouter<F>, constant: F) -> Result<Self::Num, Error> {
+        let config = self.config();
+
+        layouter.assign_region(
+            || "load constant",
+            |mut region| {
+                region.assign_advice_from_constant(
+                    ||"constant value",
+                    config.advice[0], 0,
+                    constant
+                ).map(Number)
+            },
+        )
+    }
+
+    fn mul(&self, mut layouter: impl Layouter<F>, a: Self::Num, b: Self::Num) -> Result<Self::Num, Error> {
+        let config = self.config();
+
+        layouter.assign_region(
+            || "mul",
+            |mut region: Region<'_, F>| {
+                config.s_mul.enable(&mut region, 0)?;
+                a.0.copy_advice(|| "lhs", &mut region, config.advice[0],0)?;
+                b.0.copy_advice(|| "rhs", &mut region, config.advice[1],0)?;
+
+                let value = a.0.value().copied() * b.0.value();
+                region
+                    .assign_advice(|| "lhs * rhs", config.advice[0], 1, || value)
+                    .map(Number)
+            }
+        )
+    }
+
+    fn expose_public(&self, mut layouter: impl Layouter<F>, num: Self::Num, row: usize) -> Result<(), Error> {
+        let config = self.config();
+
+        layouter.constrain_instance(num.0.cell(), config.instance, row)
+    }
+}
+
+#[derive(Default)]
+struct MulCircuit<F: FieldExt> {
+    constant: F,
+    a: Value<F>,
+    b: Value<F>,
+}
+
+impl<F: FieldExt> Circuit<F> for MulCircuit<F> {
+    type Config = FieldConfig;
+    type FloorPlanner = SimpleFloorPlanner;
+
+    fn without_witnesses(&self) -> Self {
+        Self::default()
+    }
+
+    fn configure(meta: &mut ConstraintSystem<F>) -> Self::Config {
+        let advice = [meta.advice_column(), meta.advice_column()];
+        let instance = meta.instance_column();
+        let constant = meta.fixed_column();
+
+        FieldChip::configure(meta, advice, instance, constant)
+    }
+
+    fn synthesize(&self, config: Self::Config, mut layouter: impl Layouter<F>) -> Result<(), Error> {
+        let field_chip = FieldChip::<F>::construct(config);
+        // 配置private 变量 a和b
+        let a = field_chip.load_private(layouter.namespace(|| "load a"), self.a)?;
+        let b = field_chip.load_private(layouter.namespace(|| "load b"), self.b)?;
+
+        // 配置常量
+        let constant = field_chip.load_constant(layouter.namespace(|| "load constant"), self.constant)?;
+
+        // 计算电路结果
+        let ab = field_chip.mul(layouter.namespace(|| "a * b"), a, b)?;
+        let absq = field_chip.mul(layouter.namespace(|| "ab * ab"), ab.clone(), ab)?;
+        let c = field_chip.mul(layouter.namespace(|| "constant * absq"), constant, absq)?;
+
+        // 暴露证明量c，即prover知道c的值
+        field_chip.expose_public(layouter.namespace(|| "expose c"), c, 0)
+    }
+}
+
+fn main() {
+    use halo2_proofs::{dev::MockProver, pasta::Fp};
+
+    // ANCHOR: test-circuit
+    // The number of rows in our circuit cannot exceed 2^k. Since our example
+    // circuit is very small, we can pick a very small value here.
+    let k = 4;
+
+    // Prepare the private and public inputs to the circuit!
+    let constant = Fp::from(7);
+    let a = Fp::from(2);
+    let b = Fp::from(3);
+    let c = constant * a.square() * b.square();
+
+    // Instantiate the circuit with the private inputs.
+    let circuit = MulCircuit {
+        constant,
+        a: Value::known(a),
+        b: Value::known(b),
+    };
+
+    // Arrange the public input. We expose the multiplication result in row 0
+    // of the instance column, so we position it there in our public inputs.
+    let mut public_inputs = vec![c];
+
+    // Given the correct public input, our circuit will verify.
+    let prover = MockProver::run(k, &circuit, vec![public_inputs.clone()]).unwrap();
+    assert_eq!(prover.verify(), Ok(()));
+
+    // If we try some other public input, the proof will fail!
+    public_inputs[0] += Fp::one();
+    let prover = MockProver::run(k, &circuit, vec![public_inputs]).unwrap();
+    assert!(prover.verify().is_err());
+    // ANCHOR_END: test-circuit
+}
+
+// check https://github.com/zcash/halo2/blob/47f25ad632f2a2a5a0288db466de28f8efbf22a2/halo2_proofs/examples/simple-example.rs
