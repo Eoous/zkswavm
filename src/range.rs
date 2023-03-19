a1763caf932b5a0afa86401d79359ba1c0a0c638
0xEclair <38656355+0xEclair@users.noreply.github.com>
2023-03-19 16:22:31 +0800

    feat: range chip

---
+++ src/range.rs
@@ -0,0 +1,60 @@
+use std::marker::PhantomData;
+use halo2_proofs::arithmetic::FieldExt;
+use halo2_proofs::circuit::{Layouter, Value};
+use halo2_proofs::plonk::{ConstraintSystem, Error, Expression, TableColumn, VirtualCells};
+
+pub struct RangeConfig<F: FieldExt> {
+    cols: [TableColumn; 1],
+    _mark: PhantomData<F>,
+}
+
+impl<F: FieldExt> RangeConfig<F> {
+    pub fn configure(cols: [TableColumn; 1]) -> Self {
+        RangeConfig {
+            cols,
+            _mark: PhantomData,
+        }
+    }
+
+    pub fn configure_in_range(
+        &self,
+        meta: &mut ConstraintSystem<F>,
+        expr: impl FnOnce(&mut VirtualCells<'_, F>) -> Expression<F>,
+    ) {
+        meta.lookup(|meta| vec![(expr(meta), self.cols[0])]);
+    }
+}
+
+pub struct RangeChip<F: FieldExt> {
+    config: RangeConfig<F>,
+    _phantom: PhantomData<F>,
+}
+
+impl<F: FieldExt> RangeChip<F> {
+    pub fn new(config: RangeConfig<F>) -> RangeChip<F> {
+        RangeChip {
+            config,
+            _phantom: PhantomData,
+        }
+    }
+
+    pub fn init(&self, layouter: &mut impl Layouter<F>, range: usize) -> Result<(), Error> {
+        layouter.assign_table(
+            || "common range table",
+            |mut table| {
+                for i in 0..range {
+                    table.assign_cell(
+                        || "range table",
+                        self.config.cols[0],
+                        i,
+                        || Value::known(F::from(i as u64)),
+                    )?;
+                }
+
+                Ok(())
+            }
+        )?;
+
+        Ok(())
+    }
+}
