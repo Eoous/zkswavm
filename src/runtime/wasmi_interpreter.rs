94241ef4baa912dffdb99744cae7132d3f9821e4
0xEclair <38656355+0xEclair@users.noreply.github.com>
2023-04-11 17:18:00 +0800

    feat: compile wat by wasmi

--- src/runtime/wasmi_interpreter.rs
+++ src/runtime/wasmi_interpreter.rs
@@ -1,46 +1,65 @@
-use wasmi::RuntimeValue;
+use wasmi::{ImportsBuilder, ModuleInstance, RuntimeValue};
+
 use crate::{
-    runtime::types::{
-        Value,
+    runtime::{
+        types::{CompileError, ExecutionError, Value},
+        CompileOutcome, ExecutionOutcome, WasmRuntime,
     },
-    runtime::WasmRuntime,
+    spec::{instruction::InstructionEntry, CompileTable},
 };
-use crate::runtime::{CompileOutcome, ExecutionOutcome};
-use crate::runtime::types::{CompileError, ExecutionError};
 
 pub struct WasmiRuntime {}
 
 impl From<Value> for RuntimeValue {
     fn from(value: Value) -> RuntimeValue {
         match value {
-            Value::i32(value) => {
-                RuntimeValue::from(value)
-            },
-            Value::u32(value) => {
-                RuntimeValue::from(value)
-            },
-            Value::i64(value) => {
-                RuntimeValue::from(value)
-            },
-            Value::u64(value) => {
-                RuntimeValue::from(value)
-            },
+            Value::i32(value) => RuntimeValue::from(value),
+            Value::u32(value) => RuntimeValue::from(value),
+            Value::i64(value) => RuntimeValue::from(value),
+            Value::u64(value) => RuntimeValue::from(value),
         }
     }
 }
 
 impl WasmRuntime for WasmiRuntime {
-    type Module = ();
+    type Module = wasmi::Module;
 
-    fn new() -> Self {
-        todo!()
+    fn new() -> WasmiRuntime {
+        WasmiRuntime {}
     }
 
     fn compile(&self, textual_repr: &str) -> Result<CompileOutcome<Self::Module>, CompileError> {
-        todo!()
+        let binary = wabt::wat2wasm(&textual_repr).expect("failed to parse wat.");
+        let module = wasmi::Module::from_buffer(&binary).expect("failed to load wasm binary.");
+
+        let instance = ModuleInstance::new(&module, &ImportsBuilder::default())
+            .expect("failed to instantiate wasm module.")
+            .assert_no_start();
+
+        let mut tracer = wasmi::tracer::Tracer::default();
+        tracer.register_module_instance(&instance);
+
+        Ok(CompileOutcome {
+            textual_repr: textual_repr.to_string(),
+            module,
+            tables: CompileTable {
+                instructions: tracer
+                    .itable
+                    .0
+                    .iter()
+                    .map(|inst| InstructionEntry::from(inst))
+                    .collect(),
+                init_memory: vec![], // todo
+            },
+        })
     }
 
-    fn run(&self, compile_outcome: &CompileOutcome<Self::Module>, function_name: &str, args: Vec<Value>) -> Result<ExecutionOutcome, ExecutionError> {
+    fn run(
+        &self,
+        compile_outcome: &CompileOutcome<Self::Module>,
+        function_name: &str,
+        args: Vec<Value>,
+    ) -> Result<ExecutionOutcome, ExecutionError> {
         todo!()
     }
 }
